fb624941e9abf001e8c37765b49962e43462df18
shark0der <commits@shark0der.com>
2025-12-11 10:55:21 +0200

    chore: simplify `getDataSource`

--- apps/api/src/db.ts
+++ apps/api/src/db.ts
@@ -1,17 +1,15 @@
-import { createDataSource, DataSourceOptions } from '@nexusmutual/db-schema';
+import { createDataSource } from '@nexusmutual/db-schema';
 import { DataSource } from 'typeorm';
 
 import config from './config';
 
 export const getDataSource = async (): Promise<DataSource> => {
-  const dbOptions: DataSourceOptions = {
+  return createDataSource({
     type: 'postgres',
     host: config.get('db_host'),
     port: config.get('db_port'),
     database: config.get('db_name'),
     username: config.get('db_username'),
     password: config.get('db_password'),
-  };
-
-  return createDataSource(dbOptions);
+  });
 };
